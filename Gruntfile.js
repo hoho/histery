1bfd2a910a47fc946f4cd9935bd5c390e86a1dac
Marat Abdullin <dakota@brokenpipe.ru>
2013-12-18 04:28:33 +0400

    No need for regexps here.

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,12 +42,12 @@ module.exports = function(grunt) {
     grunt.registerTask('testInit', function() {
         var express = require('express');
         webserver = express();
-        webserver.get(new RegExp('^/histery.js'), function(req, res) { res.sendfile('histery.js'); });
-        webserver.get(new RegExp('^/qunit.css'), function(req, res) { res.sendfile('node_modules/qunitjs/qunit/qunit.css'); });
-        webserver.get(new RegExp('^/qunit.js'), function(req, res) { res.sendfile('node_modules/qunitjs/qunit/qunit.js'); });
-        webserver.get(new RegExp('^/jquery.js'), function(req, res) { res.sendfile('node_modules/jquery-browser/lib/jquery.js'); });
-        webserver.get(new RegExp('^/test.js'), function(req, res) { res.sendfile('test/test.js'); });
-        webserver.get(new RegExp('^/'), function(req, res) { res.sendfile('test/index.html'); });
+        webserver.get('/histery.js', function(req, res) { res.sendfile('histery.js'); });
+        webserver.get('/qunit.css', function(req, res) { res.sendfile('node_modules/qunitjs/qunit/qunit.css'); });
+        webserver.get('/qunit.js', function(req, res) { res.sendfile('node_modules/qunitjs/qunit/qunit.js'); });
+        webserver.get('/jquery.js', function(req, res) { res.sendfile('node_modules/jquery-browser/lib/jquery.js'); });
+        webserver.get('/test.js', function(req, res) { res.sendfile('test/test.js'); });
+        webserver.get('/', function(req, res) { res.sendfile('test/index.html'); });
         webserver = webserver.listen(3000);
         grunt.log.writeln('Listening on port 3000');
     });
